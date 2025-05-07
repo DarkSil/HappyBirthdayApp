0c1a249d2b01ad6b9cbdc4b1cf7a2e3b37426fbd
DarkSil <marenkov.ilio@gmail.com>
2025-05-07 17:14:54 +0200

    Android Version Increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sli.happybirthdayapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sli.happybirthdayapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
